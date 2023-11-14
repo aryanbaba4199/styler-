2ddc1e32c57e3e27dbd1c02299de0e838eec5876
Aryan Dubey <aryanbaba@gmail.com>
2023-11-14 21:50:18 +0530

    h

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
         <meta name="google-site-verification" content="google5d6bde75488aa843" />
+        <meta name="msvalidate.01" content="07D0EE7A15FC1B6280F3A12F401BE094" />
 
           <link rel="icon" href="/favicon.png" />
         </Head>
